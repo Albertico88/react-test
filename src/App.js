a93f405c3b72a8336147b3202b4f2e243dda9626
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-07-11 19:29:26 -0400

    WIP: sass styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import './App.css';
+
 
 class App extends Component {
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 
 class Header extends Component {
@@ -9,9 +10,38 @@ class Header extends Component {
     return (
 
 
-      <div className="App">
-        My Header 
-      </div>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a className="navbar-item logo" href="#">Logo Here</a>
+
+          <div className="navbar-burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <a className="navbar-item">Home</a>
+            <a className="navbar-item">FAQ</a>
+            <a className="navbar-item">About</a>
+            <a className="navbar-item">Contact</a>
+
+          <div className="navbar-item">
+            <p className="control">
+              <a className="button is-primary is-outlined">
+                <span className="icon">
+                  <i className="fa fa-thumbs-up"></i>
+                </span>
+                <span>Join Now!</span>
+              </a>
+            </p>
+          </div>
+
+          </div>
+        </div>
+      </nav>
 
 
     );
